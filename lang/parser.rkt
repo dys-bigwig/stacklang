7bd01f2acb558c042570367152e9d070e2783629
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2020-01-03 05:39:23 +0000

    Add files via upload

--- lang/parser.rkt
+++ lang/parser.rkt
@@ -9,6 +9,8 @@
 
 (provide stack-parse)
 
+(struct quoted (es) #:prefab)
+
 (define identifier/p
   (do [id <- ((pure string->symbol) (token/p 'IDENTIFIER))]
       (pure `(identifier ,id))))
@@ -25,7 +27,7 @@
 		(token/p 'OPEN-BRACE)
 		[exprs <- (many/p expr/p)]
 		(token/p 'CLOSE-BRACE)
-		(pure `(quotation ,exprs))))
+		(pure `(quotation ,@(map quoted exprs)))))
 
 (define expr/p
 	(syntax/p (or/p quotation/p identifier/p number/p)))
